dd1f31af63103caf7b3b17538d156651719f0fce
yepizrene <yepizrene@gmail.com>
2023-05-02 20:38:59 -0700

    Added sqlScript for table  of cfdi registers

---
+++ src/main/resources/io/yepizrene/cfdigenerator/resources/scripts/dataBaseScript.sql
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  rene
+ * Created: 2/05/2023
+ */
+
+CREATE TABLE facturasCFDI(
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 100, INCREMENT BY 1),
+    serie VARCHAR(20) NOT NULL,
+    folio VARCHAR(60) NOT NULL,
+    fechaCreacion VARCHAR(25) NOT NULL,
+    fechaTimbrado VARCHAR(25) NOT NULL,
+    tipo VARCHAR(1) NOT NULL,
+    rfc VARCHAR(13) NOT NULL,
+    uuid VARCHAR(36) NOT NULL,
+    xmlPath VARCHAR(255) NOT NULL,
+    pdfPath VARCHAR(255) NOT NULL,
+    subtotal DOUBLE NOT NULL,
+    iva DOUBLE NOT NULL,
+    importe DOUBLE NOT NULL,
+    CONSTRAINT primary_key PRIMARY KEY (id)
+)
